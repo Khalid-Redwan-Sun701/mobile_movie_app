9601581685dcf18737413ea17e20b46673782dc1
Khalid Redwan Sun <khalid.redwan.sun701@gmail.com>
2025-11-05 23:55:59 -0700

    Add icon and image constants, define movie interfaces, and declare PNG module, tab routing

---
+++ app/(tabs)/saved.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Saved = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-2xl text-dark-200 font-bold">Saved</Text>
+    </View>
+  );
+};
+
+export default Saved;
